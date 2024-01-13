7b4763d309c6916409470a67d0ad2c7e106da88c
newworld <newworld1423@naver.com>
2024-01-13 12:54:58 +0900

    선언전 렌더링

--- svelte/svelte-app/src/App.svelte
+++ svelte/svelte-app/src/App.svelte
@@ -1,17 +1,33 @@
 <script>
-	import Hello from './Hello.svelte'
 	let name = 'world';
+	let age = 85;
+	
+	/*setTimeout(() => {
+		name = 'Heropy';
+		age = 36;
+	}, 3000)*/
+
+	function assign() {
+		name = 'Heropy';
+		age = 36;
+	}
 </script>
 
 <h1>Hello {name}!</h1>
-<Hello />
+<h2 class={age < 85 ? 'active' : ''}>
+	{age}
+</h2>
+<img src="" alt={name} />
+<input type="text" bind:value={name} />
+<button on:click={assign}>
+	Assign
+</button>
 
 <style>
 	h1 {
 		color: red;
-	}	
+	}
+	.active {
+		color: blue;
+	}
 </style>
-
-<button on:click={() => {name = 'Heropy'}}>
-	Change!
-</button>
