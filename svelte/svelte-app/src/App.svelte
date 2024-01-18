bb3d59ab97bdb2eab8baa3aa9c53ea183a115981
newworld <newworld1423@naver.com>
2024-01-18 21:27:12 +0900

    컴포넌트

--- svelte/svelte-app/src/App.svelte
+++ svelte/svelte-app/src/App.svelte
@@ -1,11 +1,13 @@
 <script>
 	//import {onMount} from 'svelte'
+	import Fruits from './Fruits.svelte'
 	let name = 'Fruits';
 	let age = 85;
 	let toggle = false;
 	let fruits = ['Apple', 'Banana', 'Cherry', 'Orange', 'Mango']
 	let isRed = false;
-	let text = ''
+	let text = '';
+
 	//onMount(() => {
 	//	const box = document.querySelector('.box');
 	//	box.addEventListener('click', () => {isRed = !isRed});
@@ -39,6 +41,11 @@
 		fruits = fruits.slice(1);
 	}
 </script>
+
+<Fruits {fruits} />
+<Fruits {fruits} reverse />
+<Fruits {fruits} slice="-2" />
+<Fruits {fruits} slice="0, 3" />
 
 <button on:click={() => {toggle = !toggle}}>
 	Toggle
@@ -78,6 +85,37 @@
 <button on:click={() => {text = 'Heropy'}}>
 	Click
 </button>
+
+<h2>
+	Fruits
+</h2>
+<ul>
+	{#each fruits as fruit}
+		<li>{fruit}</li>
+	{/each}
+</ul>
+
+<h2>
+	Fruits reverse
+</h2>
+<ul>
+	<!-- ...['Apple', 'Banana', 'Cherry', 'Orange', 'Mango']
+	'Apple', 'Banana', 'Cherry', 'Orange', 'Mango'
+	['Apple', 'Banana', 'Cherry', 'Orange', 'Mango']
+ 	-->
+	{#each [...fruits].reverse() as fruit}
+		<li>{fruit}</li>
+	{/each}
+</ul>
+
+<h2>
+	Fruits slice -2
+</h2>
+<ul>
+	{#each fruits.slice(-2) as fruit}
+		<li>{fruit}</li>
+	{/each}
+</ul>
 
 <style>
 	.box{
