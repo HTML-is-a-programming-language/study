5172aec26501618ae9e9f897c733ceec20bb7ec3
newworld <newworld1423@naver.com>
2024-01-18 19:49:23 +0900

    조건문과 반복문

--- svelte/svelte-app/src/App.svelte
+++ svelte/svelte-app/src/App.svelte
@@ -1,7 +1,15 @@
 <script>
-	let name = 'world';
+	let name = 'Fruits';
 	let age = 85;
-	
+	let toggle = false;
+	let fruits = ['Apple', 'Banana', 'Cherry', 'Orange', 'Mango']
+
+	if (toggle) {
+
+	} else {
+
+	}
+
 	/*setTimeout(() => {
 		name = 'Heropy';
 		age = 36;
@@ -11,9 +19,33 @@
 		name = 'Heropy';
 		age = 36;
 	}
+
+	function deleteFruit() {
+		fruits = fruits.slice(1);
+	}
 </script>
 
+<button on:click={() => {toggle = !toggle}}>
+	Toggle
+</button>
+
+{#if toggle}
+	<h1>Hello {name}!</h1>
+{:else}
+	<div>No name!</div>
+{/if}
+
 <h1>Hello {name}!</h1>
+<ul>
+	{#each fruits as fruit}
+		<li>{fruit}</li>
+	{/each}
+</ul>
+<button on:click={deleteFruit}>
+	Eat it!
+</button>
+
+
 <h2 class={age < 85 ? 'active' : ''}>
 	{age}
 </h2>
