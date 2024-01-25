20bfb8d7078608fef56d80937a04fe1bc8e140d9
newworld <newworld1423@naver.com>
2024-01-25 21:20:46 +0900

    코딩애플 리액트 2강 JSX 문법

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <div className="black-nav"> {/* JSX 문법 1 : className */}
-        <h4 style={{color:'red', fontSize:'16px'}}>블로그임</h4>
+        <h4 style={{color:'red', fontSize:'16px'}}>블로그임</h4> {/* JSX 문법 3 : style 넣을 땐 style={{스타일명 : '값'}} font-size 같이 하이픈이 포함된건 뒤의 단어에서 대문자 */}
       </div>
       <h4>{ post }</h4> {/* JSX 문법 2 : 데이터바인딩은(변수넣을 땐) 중괄호 */}
     </div>
