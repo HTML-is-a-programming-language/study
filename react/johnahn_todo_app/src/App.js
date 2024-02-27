6757571a78c27eac53b92454ca59114e95388f82
newworld <newworld1423@naver.com>
2024-02-27 20:24:47 +0900

    41강

--- react/johnahn_todo_app/src/App.js
+++ react/johnahn_todo_app/src/App.js
@@ -3,15 +3,18 @@ import "./App.css";
 import Lists from "./components/Lists";
 import Form from "./components/Form";
 
+const initialTodoData = localStorage.getItem('todoData') ? JSON.parse(localStorage.getItem('todoData')): [];
+
 export default function App() {
 
-  const [todoData, setTodoData] = useState([]);
+  const [todoData, setTodoData] = useState(initialTodoData);
   const [value, setValue] = useState("");
 
   const handleClick = useCallback((id) => {
     let newTodoData = todoData.filter(data => data.id !== id);
     console.log('newTodoData', newTodoData);
     setTodoData(newTodoData)
+    localStorage.setItem('todoData', JSON.stringify(newTodoData));
   },[todoData]);
 
   const handleSubmit = (e) => {
@@ -26,14 +29,21 @@ export default function App() {
 
     //원래 있던 할 일에 새로운 할 일 더해주기
     setTodoData(prev => [...prev, newTodo]);
+    localStorage.setItem('todoData', JSON.stringify([...todoData, newTodo]));
     setValue("");
   };
+
+  const handleRemoveClick = () => {
+    setTodoData([]);
+    localStorage.setItem('todoData', JSON.stringify([]));
+  }
 
   return(
     <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
       <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
         <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
 
         <Lists handleClick={handleClick} todoData={todoData} setTodoData={setTodoData} />
