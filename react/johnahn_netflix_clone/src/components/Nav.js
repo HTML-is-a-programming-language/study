3317a539fd29f8654ac6ad9b04b13772495c7b5d
newworld <newworld1423@naver.com>
2024-03-06 21:59:09 +0900

    50강

--- react/johnahn_netflix_clone/src/components/Nav.js
+++ react/johnahn_netflix_clone/src/components/Nav.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Nav.css"
 
 export default function Navigation() {
+  const [show, setshow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      console.log(window.scrollY);
+      if(window.scrollY > 50) {
+        setshow(true);
+      } else {
+        setshow(false);
+      }
+    })
+
+    return () => {
+      window.removeEventListener('scroll', () => {})
+    }
+  }, [])
+
   return (
-    <nav>
+    <nav className={`nav ${show && 'nav__black'}`}>
         <img alt='Netflix logo' src='http://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png' className='nav__logo' onClick={() => window.location.reload()} />
         <img alt='User logged' src='https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41' className='nav__avatar' />
     </nav>
