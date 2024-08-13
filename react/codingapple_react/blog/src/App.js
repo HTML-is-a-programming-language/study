de9d826135e541f1cbff5d87cfa20c663406bd4d
newworld <newworld1423@naver.com>
2024-08-13 20:07:12 +0900

    array, object state 변경하는 법

--- react/codingapple_react/blog/src/App.js
+++ react/codingapple_react/blog/src/App.js
@@ -7,7 +7,7 @@ import { useState } from 'react';
 function App() {
 
   let post = '강남 우동 맛집';
-  let [글제목, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
   console.log(글제목);
   let [따봉, 따봉변경] = useState(0);
 
@@ -16,8 +16,24 @@ function App() {
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
+
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy.sort();
+        글제목변경(copy);
+      }}>가나다순정렬</button>
+
+      <button onClick={()=>{
+        let arr = [1,2,3]; // array, object는 데이터를 저장하는게 아니라 데이터의 위치를 저장함 reference data type
+
+        let copy = [...글제목]; // ...은 괄호를 벗기는 문법
+        copy[0] = '여자코트 추천'
+        console.log(copy == 글제목);
+        글제목변경(copy);
+      }}>글수정</button>
+
       <div className="list">
-        <h4>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> {따봉} <span onClick={()=>{ b(글제목 = ['여자 코트 추천', '강남 우동맛집', '파이썬독학']) }}>😊</span> </h4>
+        <h4>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
@@ -28,8 +44,13 @@ function App() {
         <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <Modal/>
+
     </div>
   );
 }
+
+
 
 export default App;
