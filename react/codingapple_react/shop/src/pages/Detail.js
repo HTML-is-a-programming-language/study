4129070b2e2e22d3f13c9056ac0dda1555c68320
newworld <newworld1423@naver.com>
2024-09-05 20:24:29 +0900

    Lifecycle과 useEffect 1

--- react/codingapple_react/shop/src/pages/Detail.js
+++ react/codingapple_react/shop/src/pages/Detail.js
@@ -1,6 +1,33 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+
+// class Detail2 extends React.Component {
+//   componentDidMount(){
+
+//   }
+//   componentDidUpdate(){
+
+//   }
+//   componentWillUnmount(){
+
+//   }
+// }
+
 function Detail(props){
+
+  useEffect(() =>{
+    // 렌더링 후에 동작
+    // 오래걸리는 반복연산, 서버에서 데이터가져오는 작업, 타이머
+    // for (var i = 0; i < 10000; i++){
+    //   console.log(1);
+    // }
+    setTimeout(()=>{
+      document.querySelector('.alert').style.display = 'none';
+    }, 2000)
+  })
+
+  let [count, setCount] = useState(0)
 
     let {id} = useParams();
     let 찾은상품 = props.shoes.find(function(x){
@@ -9,6 +36,11 @@ function Detail(props){
 
     return (
       <div className="container">
+        <div className="alert alert-warning">
+          2초이내 구매시 할인
+        </div>
+        {count}
+        <button onClick={()=>{ setCount(count+1)}}>버튼</button>
         <div className="row">
           <div className="col-md-6">
             <img src={'https://codingapple1.github.io/shop/shoes'+(찾은상품.id + 1)+'.jpg'} width="100%" />
