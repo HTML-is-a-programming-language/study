494aaafed4054a2dca20023e0fc3953bec41c9d8
newworld <newworld1423@naver.com>
2024-09-12 22:08:23 +0900

    리액트에서 탭 UI 만들기 + 멋있게 컴포넌트 전환 애니메이션 주는 법 (transition)

--- react/codingapple_react/shop/src/pages/Detail.js
+++ react/codingapple_react/shop/src/pages/Detail.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Nav } from 'react-bootstrap';
 
 
 // class Detail2 extends React.Component {
@@ -23,11 +24,16 @@ function Detail(props){
   let [count, setCount] = useState(0);
   //let [alert, setAlert] = useState(true);
   let [num, setNum] = useState('');
+  let [탭, 탭변경] = useState(0);
+  let [show, setShow] = useState('');
 
   useEffect(() =>{
     if (isNaN(num) == true){
       alert('그러지마세요') // alert state가 정의되어 있어서 작동하지 않았음
     }
+
+    // 리액트의 automatic batching 기능
+    setTimeout(()=>{setShow('end')}, 100)
 
     // 렌더링 후에 동작
     // 오래걸리는 반복연산, 서버에서 데이터가져오는 작업, 타이머
@@ -44,11 +50,13 @@ function Detail(props){
     return ()=>{
       console.log(1);
       //clearTimeout(a);
+      setShow('')
     }
     // useEffect 실행조건 넣을 수 있는 곳은 []
     // count라는 state가 변할 때만 실행됨
     // state가 없을 경우에는 mount에만 실행됨
     // 컴포넌트 mount시 1회만 실행하고 싶으면 []
+
   }, [count, num])
 
   // useEffect(()=>{}) 1. 재렌더링마다 코드실행하고 싶으면
@@ -62,7 +70,7 @@ function Detail(props){
 
 
   return (
-    <div className="container">
+    <div className={'container start ' + show}>
       {/* {
         alert == true
         ? <div className="alert alert-warning">
@@ -85,8 +93,78 @@ function Detail(props){
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+
+      {/* 탭 UI 만들기
+      1. html css로 미리 디자인
+      2. 탭 상태 저장해둘 state 필요
+      3. state에 따라서 UI가 어떻게 보일지 작성
+       */}
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link eventKey="link0" onClick={()=>{ 탭변경(0) }}>버튼0</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link1" onClick={()=>{ 탭변경(1) }}>버튼1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link2" onClick={()=>{ 탭변경(2) }}>버튼2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      {/* 삼항연산자를 여러개쓰려면 중괄호를 따로 열어줘야함
+      {
+        탭 == 0 ? <div>내용0</div> : null
+      }
+      {
+        탭 == 1 ? <div>내용1</div> : null
+      }
+      {
+        탭 == 2 ? <div>내용2</div> : null
+      } */}
+      <TabContent 탭={탭}/>
     </div>
   )
 }
+
+//Q. 일반 if 조건문 쓰려면?
+//(팁1) props.어쩌구가 귀찮으면 props 대신 {탭}
+// function TabContent(props){
+//   if (props.탭 == 0){
+//     return <div>내용0</div>
+//   }
+//   if(props.탭 == 1){
+//     return <div>내용1</div>
+//   }
+//   if(props.탭 == 2){
+//     return <div>내용2</div>
+//   }
+// }
+
+function TabContent({탭}){
+  // (팁2) 센스좋으면 if 필요없을 수도
+  // 4. 원할때 end 부착하면 끝임, 탭 state가 변할 때 end 부착
+
+  let [fade, setFade] = useState('');
+
+  useEffect(()=>{
+    // 리액트의 automatic batching 기능
+    setTimeout(()=>{setFade('end')}, 100)
+
+    return ()=>{
+      setFade('')
+    }
+  }, [탭]) // 탭이라는게 변경될 때마다 안의 코드 실행해줌
+
+  return <div className={'start ' + fade}>
+    {[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][탭]}
+  </div>
+}
+
+// 전환애니메이션은
+// 부착하면 애니메이션이 나오는 className 하나 만들고
+// 원할 때 부착하면 됩니다
+// 1. 애니메이션 동작 전 className 만들기
+// 2. 애니메이션 동작 후 className 만들기
+// 3. className에 transition 속성 추가
+// 4. 원할 때 2번 className 부착
 
 export default Detail;
